042481acd484c38f744c53488138b118a50e5779
Angel <angela.s.takezawa@gmail.com>
2026-06-20 15:42:32 -0300

    feat: adiciona tela de login cadastro e dashboard

---
+++ Views/LoginView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace NextBand.Views;
+
+public partial class LoginView : UserControl
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/RegisterView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace NextBand.Views;
+
+public partial class RegisterView : UserControl
+{
+    public RegisterView()
+    {
+        InitializeComponent();
+    }
+}
