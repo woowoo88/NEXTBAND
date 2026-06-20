dd96a5cad7ed4db334be438c30a5a980e8d419aa
Angel <angela.s.takezawa@gmail.com>
2026-06-20 15:42:39 -0300

    feat: cria telas de perfil edicao e conexoes

---
+++ Views/ConnectionsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace NextBand.Views;
+
+public partial class ConnectionsView : UserControl
+{
+    public ConnectionsView()
+    {
+        InitializeComponent();
+    }
+}
