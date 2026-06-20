a42d8243bf0abeddb9c352095a2ac9a425d7fd8e
Angel <angela.s.takezawa@gmail.com>
2026-06-20 15:42:49 -0300

    feat: implementa tela My Band com LED RGB OLED e link rapido

---
+++ Views/MyBandView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace NextBand.Views;
+
+public partial class MyBandView : UserControl
+{
+    public MyBandView()
+    {
+        InitializeComponent();
+    }
+}
